5b4a3496ddfe62da642f0e0443afb0ba9c480fd1
somethingtho <dungthhe170357@fpt.edu.vn>
2024-10-31 12:10:14 +0700

    news for cinema

--- WebClient/Pages/Cinema/Index.cshtml.cs
+++ WebClient/Pages/Cinema/Index.cshtml.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebClient.Models;
 
 namespace WebClient.Pages.Cinema
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly HttpClient _httpClient;
+
+        public IndexModel(HttpClient httpClient)
         {
+            _httpClient = httpClient;
         }
+
+        public IList<Movie> MoviesNow { get; set; }
+        public IList<Movie> MoviesUp { get; set; }
+        public IList<News> News { get; set; }
     }
 }
--- WebClient/Pages/Cinema/News.cshtml.cs
+++ WebClient/Pages/Cinema/News.cshtml.cs
@@ -1,12 +1,21 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebClient.Models;
 
 namespace WebClient.Pages.Cinema
 {
     public class NewsModel : PageModel
     {
-        public void OnGet()
+        private readonly HttpClient _httpClient;
+
+        public NewsModel(HttpClient httpClient)
         {
+            _httpClient = httpClient;
+        }
+        public IList<News> News { get; set; }
+        public async Task OnGetAsync()
+        {
+            var data = await _httpClient.GetFromJsonAsync<IList<News>>("http://localhost:5280/api/News/GetAllNews");
+            News = data.OrderByDescending(x => x.PublishedDate).ToList();
         }
     }
 }
