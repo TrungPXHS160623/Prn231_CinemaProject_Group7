01da6911451ea175155cd4d2aa04041681992817
PIGB0SS <107063427+PIGB0SS@users.noreply.github.com>
2024-11-12 05:48:42 +0700

    Load Movie
    Load Movie

---
+++ WebClient/DTO/MovieDTO.cs
@@ -0,0 +1,21 @@
+﻿using WebClient.Models;
+namespace WebClient.DTO
+{
+    public class MovieDTO
+    {
+        public int? MovieId { get; set; }
+        public string? Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public int Duration { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string? Language { get; set; }
+        public string? Director { get; set; }
+        public string? Cast { get; set; }
+        public string? AgeRating { get; set; }
+        public string? TrailerUrl { get; set; }
+        public string? PosterUrl { get; set; }
+        public bool? IsActive { get; set; }
+        public List<CategoryDTO>? Categories { get; set; }
+
+    }
+}
